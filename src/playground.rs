e2a6b32f16fcc5fcb9107b853d1cf658468aea31
Karthik Karanth <karanth.karthik@gmail.com>
2019-07-05 00:52:41 +0530

    import kiss3d only if not wasm

--- src/playground.rs
+++ src/playground.rs
@@ -1,5 +1,8 @@
 extern crate spherro;
 
+
+// This is just a binary target for easy profiling with perf.
+// Ideally I'd use cargo bench to generate this binary...
 fn main() {
     let mut universe = spherro::Universe::new(
         600.0, 600.0, spherro::initializer::Strategy::DAMBREAK,
