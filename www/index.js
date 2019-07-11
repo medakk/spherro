3571989f71ef3989dcfe7bf1c1d460f54789fc39
Karthik Karanth <karanth.karthik@gmail.com>
2019-07-11 15:22:25 +0530

    single index.js

--- www/index.js
+++
@@ -1,4 +0,0 @@
-//TODO: Figure how to get rid of this file, let all
-// source code be in the `src` directory.
-import { main } from "./src/index";
-main();
--- www/src/index.js
+++ www/src/index.js
@@ -3,42 +3,39 @@ import { Universe, Strategy } from "spherro";
 import Renderer from "./renderer";
 import FPSCounter from "./fpscounter"
 
-export function main() {
-    const WIDTH = 700;
-    const HEIGHT = 700;
+const WIDTH = 700;
+const HEIGHT = 700;
 
-    const strategy = Strategy.DAMBREAK;
-    var universe = Universe.new(WIDTH, HEIGHT, strategy);
-    const canvas = document.getElementById('spherro-canvas');
-    const fpsCounter = new FPSCounter(10);
-    const renderer = new Renderer(canvas, WIDTH, HEIGHT, universe.get_size());
+const strategy = Strategy.DAMBREAK;
+var universe = Universe.new(WIDTH, HEIGHT, strategy);
+const canvas = document.getElementById('spherro-canvas');
+const fpsCounter = new FPSCounter(10);
+const renderer = new Renderer(canvas, WIDTH, HEIGHT, universe.get_size());
 
-    var shouldReset = false;
+var shouldReset = false;
 
-    const renderLoop = (currentTime) => {
-        fpsCounter.register(currentTime);
-        renderer.draw(universe, currentTime);
+const renderLoop = (currentTime) => {
+    fpsCounter.register(currentTime);
+    renderer.draw(universe, currentTime);
 
-        for(var i=0; i<5; i++) {
-            universe.update(0.002);
-        }
+    for(var i=0; i<5; i++) {
+        universe.update(0.002);
+    }
 
-        if(shouldReset) {
-            universe = Universe.new(WIDTH, HEIGHT, strategy);
-            shouldReset = false;
-        }
+    if(shouldReset) {
+        universe = Universe.new(WIDTH, HEIGHT, strategy);
+        shouldReset = false;
+    }
 
-        const fps = fpsCounter.smoothFPS();
-        document.getElementById('fps').innerText = fps.toFixed(2) + ' FPS';
+    const fps = fpsCounter.smoothFPS();
+    document.getElementById('fps').innerText = fps.toFixed(2) + ' FPS';
 
-        requestAnimationFrame(renderLoop);
-    };
     requestAnimationFrame(renderLoop);
+};
+requestAnimationFrame(renderLoop);
 
-    document.addEventListener('keypress', function(e) {
-        if(e.key == 'r') {
-            shouldReset = true;
-        }
-    })
-
-}
+document.addEventListener('keypress', function(e) {
+    if(e.key == 'r') {
+        shouldReset = true;
+    }
+})
